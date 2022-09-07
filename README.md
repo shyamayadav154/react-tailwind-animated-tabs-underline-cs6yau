154a85ad1ac533e68229ac1f5c427b55d7176c76
shyamayadav154 <97844955+shyamayadav154@users.noreply.github.com>
2022-09-07 22:58:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-tailwind-animated-tabs-underline-cs6yau
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-tailwind-animated-tabs-underline-cs6yau)
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Tabs } from './Tabs';
+
+function App() {
+  return (
+    <div className="px-4 py-8">
+      <Tabs />
+    </div>
+  );
+}
+
+export default App;
